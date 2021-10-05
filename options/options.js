56287950f8904592ecf22400cfbf069d6d7643c0
Crowdbotics <team@crowdbotics.com>
2021-10-05 15:08:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "floral_limit_31118",
+  url: "https://floral_limit_31118.botics.co",
+  api: "https://floral_limit_31118.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
